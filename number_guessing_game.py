978e20edef332d33a2a45bec2ebd3ced5f2be0a0
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-10-05 16:45:36 -0400

    complete

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,2 +1,23 @@
-name = input("what is your name")
-print(name)
+import random
+import math
+
+
+num = random.randint(1, 100)
+
+count = 0
+
+while count < 8:
+    count += 1
+
+    guess = int(input("Guess a number between 1 and 100: "))
+
+    if num == guess:
+       print("Congratulations you guessed right!")
+       break
+    elif num > guess:
+       print("Too low!")
+    elif num < guess:
+       print("Too high!")
+
+    if count == 8:
+        print("You lost! The correct number was ", num)
