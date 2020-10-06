f40b44ff5d4168f7eaae988e1ae820cc5ed85a5c
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-10-06 10:44:09 -0400

    c complete

---
+++ number_guesser_c.py
@@ -0,0 +1,22 @@
+import random
+
+num = int(input("Enter a number between 1 and 100: "))
+
+counter = 0
+
+low = 1
+high = 100
+guess = 50
+
+while guess != num:
+    counter += 1
+
+    guess = (low + high)//2
+    print( guess)
+
+    if guess > num:
+        high = guess
+    elif guess < num:
+        low = guess + 1
+
+print('The computer guessed in', counter, 'tries.')
